7dd4da39bf147bffd73d584fefb8ef7f49e5b752
whitedadam <whitedadam@yahoo.com>
2022-04-05 00:00:05 -0400

    Updates to NavBar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,14 @@
-import { Navbar, NavbarText, Nav, NavItem, NavLink } from "reactstrap";
+import { Navbar, NavbarText, Nav, NavItem, NavLink, Col } from "reactstrap";
 import { Link } from "react-router-dom";
+import logo from "../images/logo.png";
 
 const Navigation = () => {
   return (
     <Navbar style={{ color: "#333" }}>
-      <NavbarText>Availity Assessment</NavbarText>
+      <Col sm={4}>
+        <img src={logo} height={40} width={40} alt="Availity Logo"></img>
+        <NavbarText>Availity Assessment</NavbarText>
+      </Col>
       <Nav>
         <NavItem>
           <NavLink tag={Link} to="/">
